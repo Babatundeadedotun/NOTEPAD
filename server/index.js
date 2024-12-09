3f52f88b485480cdff9a876a035e333d138f851a
Babatunde <babatunde.adedotun94@gmail.com>
2024-12-09 16:15:41 +0100

    commit

--- server/index.js
+++ server/index.js
@@ -36,13 +36,10 @@ app.use(express.static(path.join(__dirname, 'public')))
 
     app.use("/api/messages", messageRouter)
 
-    // app.get("/", (req, res) => {
-    //     res.send("Hello World");
-    // })
-
-    app.get("*", (req, res) => {
-        res.sendFile(path.resolve(__dirname, "public", "index.html"))
+    app.get("/", (req, res) => {
+        res.send("Hello World");
     })
+
 
 
 // app.listen(PORT,() => {
